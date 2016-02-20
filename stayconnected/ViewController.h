63d2cb5673ee429376df0cd8332c434024734f3f
SowmiaS <sowmiasundar@gmail.com>
2016-02-20 21:33:13 +0530

    Initial Commit

---
+++ stayconnected/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  stayconnected
+//
+//  Created by TCS on 20/02/16.
+//  Copyright © 2016 Sowmia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ stayconnected/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  stayconnected
+//
+//  Created by TCS on 20/02/16.
+//  Copyright © 2016 Sowmia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
